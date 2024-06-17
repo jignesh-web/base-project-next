0ccd08509ff700786c78152c5edc837b36e0c1c6
Jignesh Dabhi <jigneshdabhiseemflow@gmail.com>
2024-06-17 15:08:39 +0530

    chore: update tailwind config and add font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,50 @@
 /** @type {import('tailwindcss').Config} */
+
+const colorPalette = {
+  pink: {
+    // 50: "#0",
+    100: "#FFF1F3",
+    200: "#FFE4E6",
+    300: "#FFC1C7",
+    400: "#FF98A2",
+    500: "#FF7582",
+    600: "#FF5263",
+    700: "#CC424F",
+    800: "#98313B",
+    900: "#7A272F",
+    A100: "#5C1E24",
+  },
+  black: {
+    100: "#F3F4F7",
+    200: "#E2E4EB",
+    300: "#CCD0DC",
+    400: "#B3BACB",
+    500: "#9BA4BA",
+    600: "#828DA9",
+    700: "#626E8E",
+    800: "#49526A",
+    900: "#303646",
+    A100: "#2A2F3D",
+    A200: "#20242F",
+    A300: "#191C23",
+    A400: "#15181E",
+    A500: "#121419",
+  },
+
+  common: {
+    white: "#fff",
+    black: "#000",
+  },
+  transparent: { DEFAULT: "#00000000" },
+  primary: { DEFAULT: "#FF5263", light: "#FFF1F3", dark: "#CC424F" },
+  secondary: { DEFAULT: "#17181B", light: "#17181bcd" },
+  success: { DEFAULT: "#40C34D", dark: "#1FDC1B", light: "#0EA32E" },
+  error: { DEFAULT: "#DD5757", dark: "#642728", light: "#A04041" },
+  accent: { DEFAULT: "#f4f4f5", light: "#FFF5EE" },
+  blue: { light: "#50D5FF", DEFAULT: "#1448FF", dark: "#6597BE" },
+  green: { DEFAULT: "#4DAC2B", light: "#F1F8EE" },
+};
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,43 +53,7 @@ module.exports = {
   ],
   theme: {
     colors: {
-      border: "var(--border)",
-      input: "var(--input)",
-      ring: "var(--ring)",
-      background: "var(--background)",
-      foreground: {
-        DEFAULT: "var(--foreground)",
-        light: "var(--foreground-light)",
-      },
-      primary: {
-        DEFAULT: "var(--primary)",
-        foreground: "var(--primary-foreground)",
-        default: "var(--primary-default)",
-      },
-      secondary: {
-        DEFAULT: "var(--secondary)",
-        foreground: "var(--secondary-foreground)",
-      },
-      destructive: {
-        DEFAULT: "var(--destructive)",
-        foreground: "var(--destructive-foreground)",
-      },
-      muted: {
-        DEFAULT: "var(--muted)",
-        foreground: "var(--muted-foreground)",
-      },
-      accent: {
-        DEFAULT: "var(--accent)",
-        foreground: "var(--accent-foreground)",
-      },
-      popover: {
-        DEFAULT: "var(--popover)",
-        foreground: "var(--popover-foreground)",
-      },
-      card: {
-        DEFAULT: "var(--card)",
-        foreground: "var(--card-foreground)",
-      },
+      ...colorPalette,
     },
     screens: {
       sm: "640px",
@@ -53,13 +63,24 @@ module.exports = {
       "2xl": "1536px",
     },
     extend: {
+      fontFamily: {
+        sans: ["Poppins"],
+      },
+      fontWeight: {
+        thin: 100,
+        extralight: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      fontFamily: {
-        sans: ["Onest"],
       },
     },
   },
