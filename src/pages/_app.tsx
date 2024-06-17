7d5853ab94c67c658cc3a3430c35396c835afecb
Jignesh Dabhi <jigneshdabhiseemflow@gmail.com>
2024-06-17 15:16:04 +0530

    feat: integrate react-hot-toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,16 +4,19 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { ToastProvider } from "@/components";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <AuthHandler>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </AuthHandler>
+        <ToastProvider>
+          <AuthHandler>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </AuthHandler>
+        </ToastProvider>
       </PersistGate>
     </Provider>
   );
